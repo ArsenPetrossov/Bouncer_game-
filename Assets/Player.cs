5f75cfd65b5e8275f2211a6d32ac899b28aa8050
Mahmudi <@ArsenPetrossov>
2024-03-02 18:35:14 +0500

    feat: add move player to mousePosition

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
+
+    [SerializeField] private float _force;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToMouseDirection();    
+        }
         
+    }
+
+    private void MoveToMouseDirection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 mousePos = hit.point;
+            Vector3 direction = mousePos - transform.position;
+            
+            _rigidbody.AddForce(direction * _force, ForceMode.Impulse);
+        }
     }
 }
